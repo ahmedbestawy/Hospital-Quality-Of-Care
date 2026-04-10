6dffa223283267b30ab5fccc9f03e528e1054e24
ahmedbestawy <ahmedbestawy123@outlook.com>
2026-04-10 02:58:56 +0200

    Upload.

---
+++ Hospital Quality Of Care.R
@@ -0,0 +1,105 @@
+# Project: Ranking System for Hospital Quality and Clinical Outcomes in R.
+# Author: [Ahmed Bestawy]
+# Data Source: [https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2FProgAssignment3-data.zip]
+
+# Description : ----
+# This project gives you a strong set of R functions that can look at and rank 
+# more than 4,000 U.S. hospitals based on their 30-day mortality rates for three
+# specific clinical outcomes: heart attack, heart failure, and pneumonia.
+# The script takes raw Medicare data and finds the best hospitals at the state
+# and national levels and cleans up messy data on its own and has a built-in 
+# referee to break ties if two hospitals have the exact same mortality rate.
+
+
+# Downloading data. ----
+data_url <- "https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2FProgAssignment3-data.zip"
+if(!file.exists("outcome-of-care-measures.csv"))
+          {download.file(data_url,"data.zip")
+          unzip("data.zip")}
+
+
+
+# Reading raw data. 
+outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcomedata) 
+
+names(outcomedata)
+outcomedata[ ,11] <- as.numeric(outcome[ ,11])
+
+hist(outcomedata[ ,11])
+
+
+# Cleaning the data.----
+
+analysis_data <- data.frame(outcomedata[ ,c(2,7,11,17,23)])
+
+colnames(analysis_data) <- c("Hospital", "state","heart attack",
+                             "heart failure","pneumonia")
+
+analysis_data[ ,1:2] <- lapply(analysis_data[ ,1:2], as.character)
+
+analysis_data[ ,3:5] <- lapply(analysis_data[ ,3:5], as.numeric)
+
+ss <- split(analysis_data, analysis_data$state)
+
+
+# Finding the best hospital in a state. ----
+
+best <- function(state, outcome) {
+    if(!state %in% names(ss)) { 
+        stop("invalid state")
+    } 
+    else if(!outcome %in% names(analysis_data)){
+        stop("invalid outcome")
+    }
+    LS <- lapply(ss, function(x){
+        which.min(x[ ,outcome])})
+    
+    row_number <- LS[[state]]
+    state_data <- ss[[state]]
+    
+    state_data <- state_data[order(state_data[ , outcome]
+                                   , state_data[ ,1]), ]
+    
+    state_data[row_number,1]
+}
+
+
+# Ranking hospitals by outcome in a state ----
+
+rankhospital <- function(state, outcome, num = "best") {
+    if(!state %in% names(ss)) { 
+        stop("invalid state")
+    } 
+    else if(!outcome %in% names(analysis_data)){
+        stop("invalid outcome")
+    }
+    state_data <- ss[[state]]
+    state_data <- state_data[order(state_data[ , outcome]
+                                   , state_data[ ,1]), ]
+    
+    RL <- state_data[!is.na(state_data[ , outcome]), ]
+    if(num == "best"){
+        num <- 1
+    } else if(num == "worst") {
+        num <- nrow(RL)
+    }
+    RL[num,1]
+}
+
+
+# Ranking hospitals across all states ----
+
+rankall <- function(outcome, num = "best") {
+    n <- names(ss)
+    if(!outcome %in% names(analysis_data)){
+        stop("invalid outcome")
+    }
+    hosp_vector <- sapply(n, rankhospital, outcome, num)
+    final_output <- data.frame(hospital = hosp_vector,
+                               state = names(hosp_vector))
+    final_output
+}
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Hospital-Quality-Of-Care
+## Hospital-Quality-Of-Care
+
+### This project gives you a strong set of R functions that can look at and rank more than 4,000 U.S. hospitals based on their 30-day mortality rates for three specific clinical outcomes: heart attack, heart failure, and pneumonia. The script takes raw Medicare data and finds the best hospitals at the state and national levels and cleans up messy data on its own and has a built-in referee to break ties if two hospitals have the exact same mortality rate.
+
+### Functions
+
+|  |  |
+|----|----|
+| **Function** | **Purpose** |
+| `best()` | Identifies the single hospital with the lowest mortality rate in a given state. |
+| `rankhospital()` | Returns the hospital name at a specific rank (e.g., 5th or "worst") for a chosen outcome. |
+| `rankall()` | Compiles a state-by-state list of hospitals at a specific rank into a single, unified data frame. |
+
+### Output
+
+![](images/Screenshot%202026-04-10%20011427.png){width="304"}
+
+![](images/Screenshot%202026-04-10%20012919.png){width="350"}
